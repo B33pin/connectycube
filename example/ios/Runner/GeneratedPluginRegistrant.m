0c51bc15dd92fe9b4ff2366a478cb61334b2fc81
B33pin <bipin.bhandari1510@gmail.com>
2022-05-30 06:56:58 +0545

    custom config

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<connectycube_flutter_call_kit/ConnectycubeFlutterCallKitPlugin.h>)
+#import <connectycube_flutter_call_kit/ConnectycubeFlutterCallKitPlugin.h>
+#else
+@import connectycube_flutter_call_kit;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ConnectycubeFlutterCallKitPlugin registerWithRegistrar:[registry registrarForPlugin:@"ConnectycubeFlutterCallKitPlugin"]];
+}
+
+@end
